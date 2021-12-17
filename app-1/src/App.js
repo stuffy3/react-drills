0dfe0b13889ed561eff2cc99b132703867f6c1ab
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2021-12-17 15:08:27 -0700

    initialize

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: ''
+    }
+  }
+
+  changeHandler(value) {
+    this.setState({message: value})
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <input onChange={(e) => this.changeHandler(e.target.value)}></input>
+        <p>{this.state.message}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
