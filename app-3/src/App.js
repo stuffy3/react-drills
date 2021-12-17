f4579fe3a29b44ab25aaf98cfb6f2197efe8e346
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2021-12-17 16:03:14 -0700

    finished app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      animalArray: ['Dog', 'Cat', 'Fish', 'Lizard', 'Snake'],
+      userInput: '',
+      
+    }
+  
+  }
+
+  changeHandler(value) {
+    this.setState({userInput: value})
+  }
+
+  render() {
+    let animalsToDisplay = this.state.animalArray
+    .filter((element, index) => {
+      return element.includes(this.state.userInput)
+    }).map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+    });
+
+    return(
+      <div className="App">
+        <input onChange={(e) => this.changeHandler(e.target.value)} type="text"></input>
+        {animalsToDisplay}
+      </div>
+
+    )
+  }
+
+
+
+
 }
 
 export default App;
